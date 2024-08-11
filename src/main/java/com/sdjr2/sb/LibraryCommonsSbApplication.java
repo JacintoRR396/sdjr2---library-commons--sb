311395db1e9170d8fa758abf388692154d6df291
Jacinto R2 <balbino.v12@gmail.com>
2024-08-11 13:35:38 +0200

    init project

---
+++ src/main/java/com/sdjr2/sb/LibraryCommonsSbApplication.java
@@ -0,0 +1,13 @@
+package com.sdjr2.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryCommonsSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryCommonsSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdjr2/sb/LibraryCommonsSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdjr2.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCommonsSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
